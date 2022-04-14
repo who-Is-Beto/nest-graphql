cbae23b036a049a4a086c7f96ceaaa19b0e35f78
whoisbeto <whoisbeto.gmail.com>
2022-04-14 13:56:31 -0500

    [add] scheema and basic service behavor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PetsModule } from './pets/pets.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
