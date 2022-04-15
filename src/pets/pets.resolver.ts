a4145fd961654888a6ce8bcb3539a0c0148952a9
whoisbeto <whoisbeto.gmail.com>
2022-04-14 22:44:46 -0500

    [add] database and basic methods

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -1,7 +1,8 @@
 import { Pet } from './pet.entity';
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Resolver } from '@nestjs/graphql';
 import { PetsService } from './pets.service';
 import { Query } from '@nestjs/graphql';
+import { CreatePetInput } from './dto/createPetInput.dto';
 
 @Resolver((of) => Pet)
 export class PetsResolver {
@@ -10,5 +11,20 @@ export class PetsResolver {
   @Query((returns) => [Pet])
   public async pets(): Promise<Pet[]> {
     return this.petService.findAll();
+  }
+
+  @Mutation((returns) => Pet)
+  public async createPet(
+    @Args('createPetInput') createPetInput: CreatePetInput,
+  ): Promise<Pet> {
+    const myPet = await this.petService.createPet(createPetInput);
+    return myPet;
+  }
+
+  @Query((returns) => Pet)
+  public async findOne(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<Pet> {
+    return this.petService.findOne(id);
   }
 }
